1fee11ec1abb2bba8fd38dcb6a4ea5200fa11ff1
ranei1 <rane_i@hotmail.fi>
2025-12-07 16:15:06 +0200

    React allke props asti

---
+++ osa1/part1/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+const App = () => {
+  const nimi = "Pekka"
+  const ika = 10
+  return (
+    <div>
+      <h1>
+        Greetings
+      </h1>
+      <Hello name="Maya" age={36 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+}
+
+export default App
