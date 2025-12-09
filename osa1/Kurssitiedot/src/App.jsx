650f57caacb8cbb159c7b27fe1a10bf28594a966
ranei1 <rane_i@hotmail.fi>
2025-12-09 12:31:06 +0200

    teht1.4

--- osa1/Kurssitiedot/src/App.jsx
+++ osa1/Kurssitiedot/src/App.jsx
@@ -9,9 +9,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.part1} </p>
-      <p>{props.part2} </p>
-      <p>{props.part3} </p>
+      <p>{props.part1_2} </p>
+      <p>{props.part2_2} </p>
+      <p>{props.part3_2} </p>
     </div>
   )
 }
@@ -25,19 +25,30 @@ const Total = (props) => {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header kurssi={course} />
-      <Content part1={`${part1} ${exercises1}`} part2={`${part2} ${exercises2}`}
-        part3={`${part3} ${exercises3}`} />
-      <Total maara1={exercises1} maara2={exercises2} maara3={exercises3} />
+      {/*<Content part1_2={`${part1.name} ${part1.exercises}`} part2_2={`${part2.name} ${part2.exercises}`}
+        part3_2={`${part3.name} ${part3.exercises}`} />*/}
+      <Content
+        part1_2={part1.name + " " + part1.exercises}
+        part2_2={part2.name + " " + part2.exercises}
+        part3_2={part3.name + " " + part3.exercises}
+      />
+      <Total maara1={part1.exercises} maara2={part2.exercises} maara3={part3.exercises} />
     </div>
   )
 }
