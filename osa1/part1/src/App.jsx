b1719e876cac6707dbd96a438d8974ad0a83b178
ranei1 <rane_i@hotmail.fi>
2025-12-17 10:48:13 +0200

    1C harjoitus tehty

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,29 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-const Hello = ({ name, age }) => {
+const Display = ({ counter }) => <div> {counter} </div>
 
-  const bornYear = () => new Date().getFullYear() - age
 
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were propbly born {bornYear()}
-      </p>
-    </div>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const nimi = "Pekka"
-  const ika = 10
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const decreazeByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+
+  }
+
+  //console.log('rendering...', counter)//
   return (
     <div>
-      <h1>
-        Greetings
-      </h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='Plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreazeByOne}
+        text='minus'
+      />
     </div>
   )
 }
